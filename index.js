daf18f25f5df18385fdb4887d928d8132500ce61
Vipul Sharma <54861494+sharmavipul9756@users.noreply.github.com>
2021-07-15 00:03:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = process.env.PORT || 3000
+const port = 3000
 const app = express();
 app.set('view engine','hbs')
 app.use(express.static('public'))
